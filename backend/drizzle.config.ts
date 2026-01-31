f358230d7f3cd73c2208f61706d0870a0b4401c7
vikas-saini-7 <vikas289888@gmail.com>
2026-01-31 19:04:40 +0530

    bug: version code mismatch

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit';
 import 'dotenv/config';
 
-export default defineConfig({
+export default {
   schema: './src/database/schema/index.ts',
   out: './drizzle',
-  dialect: 'postgresql',
+  driver: 'pg',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
-});
+} satisfies Config;
